055d80cdf48a76feece843f2425a9dafedfd4e1f
James <jamesmcdaniel33@hotmail.com>
2023-06-01 12:48:41 -0500

    first solution

--- ProblematicProblem/Program.cs
+++ ProblematicProblem/Program.cs
@@ -8,58 +8,134 @@ namespace ProblematicProblem
     {
         Random rng;
         bool cont = true;
-        static List<string> activities = new List<string>() { "Movies", "Paintball", "Bowling", "Lazer Tag", "LAN Party", "Hiking", "Axe Throwing", "Wine Tasting" };
-        static void Main(string[] args)
+        static List<string> activities = new List<string>()
         {
-            Console.Write("Hello, welcome to the random activity generator! \nWould you like to generate a random activity? yes/no: ");
-            bool cont;
-            switch (Console.ReadLine().ToLower())
-            {
-                case "yes":
-                    cont = true;
-                    break;
-                case "no":
-                    cont = false;
-                    break;
-            }
-            Console.WriteLine();
-            Console.Write("We are going to need your information first! What is your name? ");
-            string userName = Console.ReadLine();
-            Console.WriteLine();
-            Console.Write("What is your age? ");
-            int userAge = int.Parse(Console.ReadLine());
-            Console.WriteLine();
-            Console.Write("Would you like to see the current list of activities? Sure/No thanks: ");
-            bool seeList = bool.Parse(Console.ReadLine());
-            if (seeList)
-            {
-                foreach (string activity in activities)
-                {
-                    Console.Write($"{activity} ");
-                    Thread.Sleep(250);
-                }
-            }
-            Console.WriteLine();
-            Console.Write("Would you like to add any activities before we generate one? yes/no: ");
-            bool addToList = bool.Parse(Console.ReadLine());
-            Console.WriteLine();
-            while (addToList)
-            {
-                Console.Write("What would you like to add? ");
-                string userAddition = Console.ReadLine();
-                activities.Add(userAddition);
-            }
-            foreach (string activity in activities)
-            {
-                Console.Write($"{activity} ");
-                Thread.Sleep(250);
-            }
-            Console.WriteLine();
-            Console.WriteLine("Would you like to add more? yes/no: ");
-            addToList = bool.Parse(Console.ReadLine());
+            "Movies",
+            "Paintball",
+            "Bowling",
+            "Lazer Tag",
+            "LAN Party",
+            "Hiking",
+            "Axe Throwing",
+            "Wine Tasting"
+        };
+        public static void GetRandomActivity()
+        {
+            Random random = new Random();
+            int randomNum = random.Next(activities.Count);
+            var randomActivity = activities[randomNum];
+            Console.WriteLine(randomActivity);
         }
 
+        static void Main(string[] args)
+        {
+            bool exitProgram = false;
+            while (!exitProgram)
+            {
+                Console.Write("Hello, welcome to the random activity generator! \nWould you like to generate a random activity? yes/no: ");
+                var userInput = Console.ReadLine().ToLower();
+
+                if(userInput == "yes")
+                {
+                    Console.WriteLine();
+
+                    GetRandomActivity();
+                    //Console.Write("We are going to need your information first! What is your name? ");
+                    //string userName = Console.ReadLine();
+                    //Console.WriteLine();
+                    //Console.Write("What is your age? ");
+                    //int userAge = int.Parse(Console.ReadLine());
+                    //Console.WriteLine();
+
+                    Console.Write("\nWould you like to see the current list of activities? Sure/No thanks: ");
+                    var input2 = Console.ReadLine().ToLower();
+                    bool exit2 = false;
+
+                    while (!exit2)
+                    {
+                        if (input2 == "sure")
+                        {
+                            Console.WriteLine();
+                            bool seeList = true;
+                            if (seeList)
+                            {
+                                foreach (string activity in activities)
+                                {
+                                    Console.Write($"{activity} ");
+                                    Thread.Sleep(250);
+                                }
+                            }
+                            Console.WriteLine();
+                            Console.Write("\nWould you like to add any activities? yes/no: ");
+                            var input3 = Console.ReadLine().ToLower();
+                            bool exit3 = false;
+
+                            while (!exit3)
+                            {
+                                if(input3 == "yes")
+                                {
+                                    Console.Write("\nWhat would you like to add? ");
+                                    string userAddition = Console.ReadLine();
+                                    activities.Add(userAddition);
+
+
+                                    Console.WriteLine("\nWould you like to add more? yes/no: ");
+                                    bool exit4 = false;
+                                    var input4 = Console.ReadLine().ToLower();
+
+                                    while (!exit4)
+                                    {
+                                        if (input4 == "yes")
+                                        {
+                                            exit4 = true;
+                                        }
+                                        else if (input4 == "no")
+                                        {
+                                            //exitProgram = true;
+                                            exit2 = true;
+                                            exit3 = true;
+                                            exit4 = true;
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("Please enter 'yes' or 'no'");
+                                            input4 = Console.ReadLine().ToLower();
+                                        }
+                                    }
+                                }     
+                                else if (input3 == "no")
+                                {
+                                    exit2 = true;
+                                    exit3 = true;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Please enter 'yes' or 'no'");
+                                    input3 = Console.ReadLine().ToLower();
+                                }
+                            }
+
+                            Console.WriteLine();
+
+                        }
+                        else if (input2 == "no thanks")
+                        {
+                            //exitProgram = true;
+                            exit2 = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Please enter 'Sure' or 'No Thanks'");
+                            input2 = Console.ReadLine().ToLower();
+                        }
+                    }
+                }
+
+                else if (userInput == "no")
+                {
+                    exitProgram = true;
+                }
+            }
+        }
     }
-
-
 }
